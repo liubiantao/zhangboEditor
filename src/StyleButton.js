4a35efa38b791b577810838348fd1e5a37719439
Haitao Yin <liubiantao@gmail.com>
2017-09-14 21:41:36 +0800

    拆分StyleButton

---
+++ src/StyleButton.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export class StyleButton extends React.Component {
+  constructor() {
+    super()
+    this.onToggle = e => {
+      e.preventDefault()
+      this.props.onToggle(this.props.style)
+    }
+  }
+
+  render() {
+    let className = 'RichEditor-styleButton'
+    if (this.props.active) {
+      className += ' RichEditor-activeButton'
+    }
+
+    return (
+      <span className={className} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    )
+  }
+}
+
+const BLOCK_TYPES = [{ label: '层级', style: 'unordered-list-item' }]
+
+export const BlockStyleControls = props => {
+  const { editorState } = props
+  const selection = editorState.getSelection()
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType()
+  console.log(blockType)
+
+  return (
+    <div className="RichEditor-controls">
+      {BLOCK_TYPES.map(type => (
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,55 +1,7 @@
 import React from 'react'
 import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js'
+import { StyleButton, BlockStyleControls } from './StyleButton'
 import './editor.css'
-
-class StyleButton extends React.Component {
-  constructor() {
-    super()
-    this.onToggle = e => {
-      e.preventDefault()
-      this.props.onToggle(this.props.style)
-    }
-  }
-
-  render() {
-    let className = 'RichEditor-styleButton'
-    if (this.props.active) {
-      className += ' RichEditor-activeButton'
-    }
-
-    return (
-      <span className={className} onMouseDown={this.onToggle}>
-        {this.props.label}
-      </span>
-    )
-  }
-}
-
-const BLOCK_TYPES = [{ label: '层级', style: 'unordered-list-item' }]
-
-const BlockStyleControls = props => {
-  const { editorState } = props
-  const selection = editorState.getSelection()
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType()
-  console.log(blockType)
-
-  return (
-    <div className="RichEditor-controls">
-      {BLOCK_TYPES.map(type => (
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      ))}
-    </div>
-  )
-}
 
 class MyEditor extends React.Component {
   constructor(props) {
